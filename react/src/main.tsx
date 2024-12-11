e017a8cffa6a5925381afb5ea90bda14511c7712
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-12-11 16:53:36 +0100

    f: Use themeProvider

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { plPL } from "@mui/material/locale";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme({}, plPL);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
