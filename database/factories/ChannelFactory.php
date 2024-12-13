4531340e70378f36d7221737675636aebd5b9813
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-12-13 17:38:28 +0100

    b: Add ChannelFactory

---
+++ database/factories/ChannelFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Channel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Channel>
+ */
+class ChannelFactory extends Factory
+{
+    protected $model = Channel::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'number' => $this->faker->unique()->numberBetween(1, 999),
+        ];
+    }
+}
